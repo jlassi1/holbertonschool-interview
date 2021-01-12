ea6d862320da90151ef99fd29f9d39e12f6a3e1a
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-12 19:29:50 +0100

    sandpiles.h

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -8,7 +8,6 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 bool verification(int a[3][3]);
-void sum(int a[3][3], int b[3][3]);
 void stable(int a[3][3], int tmp[3][3]);
 void print(int a[3][3]);
 
