4a07a0c6d0c655f56eb24146c80558a13d7cccff
jlassi1 <khawlajlassi1990@gmail.com>
2021-01-21 20:01:05 +0100

    0-is_palindrome.c

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ *is_palindrome - returns 1 if it is a palindrome and 0 if not
  *@head: double pointer to head node
  *Return: 1 if palindrome and 0 if is not
  */
