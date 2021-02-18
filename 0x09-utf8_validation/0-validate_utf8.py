aa6ffd9a766293fc8d34b43f3d9fa6032e03dd8a
jlassi1 <khawlajlassi1990@gmail.com>
2021-02-18 20:35:06 +0100

    0-validate_utf8.py

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -9,19 +9,8 @@ def validUTF8(data):
     function that determines if a given data
     set represents a valid UTF-8 encoding
     """
-    count = 0
-    for c in data:
-        if count == 0:
-            if (c >> 5) == 0b110:
-                count = 1
-            elif (c >> 4) == 0b1110:
-                count = 2
-            elif (c >> 3) == 0b11110:
-                count = 3
-            elif (c >> 7):
-                return False
-        else:
-            if (c >> 6) != 0b10:
-                return False
-            count -= 1
-        return count == 0
+    try:
+        bytes([i & ((2**8)-1) for i in data]).decode("UTF-8")
+        return True
+    except Exception:
+        return False
