05834d9a8ec132930cab23ca450ae8f1a7e7f7fb
jlassi1 <57707783+jlassi1@users.noreply.github.com>
2021-02-18 20:51:08 +0100

    Create 0-validate_utf8.py

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """
+    function that determines if a given data
+    set represents a valid UTF-8 encoding
+    """
+    count = 0
+        for c in data:
+            if count == 0:
+                if (c >> 5) == 0b110:
+                    count = 1
+                elif (c >> 4) == 0b1110:
+                    count = 2
+                elif (c >> 3) == 0b11110:
+                    count = 3
+                elif (c >> 7):
+                    return False
+            else:
+                if (c >> 6) != 0b10:
+                    return False
+                count -= 1
+        return count == 0
