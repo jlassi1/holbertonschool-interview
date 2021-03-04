832f6a003ae55a8fb9d1a09b4eabef3163ad9ce3
jlassi1 <khawlajlassi1990@gmail.com>
2021-03-04 15:08:48 +0100

    menger sponge

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "menger.h"
+#include <math.h>
+
+/**
+ * menger - Function that draws a 2D Menger Sponge
+ *
+ * @level:  is the level of the Menger Sponge to draw
+ *
+ * Return: void
+ */
+void menger(int level)
+{
+int count = pow(3, level), i, j, x, y;
+char first_sponge[] = "###\n# #\n###";
+char s = ' ';
+	if (level < 0)
+		return;
+	if (level == 0)
+		printf("#\n");
+
+	if (count == 3)
+		printf("%s\n", first_sponge);
+
+	else
+		for (i = 0; i < count; i++)
+		{
+			for (j = 0; j < count; j++)
+			{
+				s = '#';
+				x = i;
+				y = j;
+				while (x > 0 || y > 0)
+				{
+					if ((x % 3 == 1) && y % 3 == 1)
+						s = ' ';
+					x = x / 3;
+					y = y / 3;
+				}
+
+			printf("%c", s);
+			}
+
+			printf("\n");
+
+		}
+
+}
+
---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,10 @@
+#ifndef _MENGER_SPONGE_
+#define _MENGER_SPONGE_
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void menger(int level);
+
+#endif
