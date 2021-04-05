98c2eacb76264532ff1cdae73f8aa2b729b86825
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-05 22:11:07 +0100

    0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,12 +4,12 @@ const ID = process.argv[2];
 
 request({ url: ('https://swapi-api.hbtn.io/api/films/' + ID), json: true }, function (err, res, json) {
   if (err) {
-    throw err;
+    return console.log(err);
   }
   for (const k of json.characters) {
     request({ url: k, json: true }, function (err, res, json) {
       if (err) {
-        throw err;
+        return console.log(err);
       }
       console.log(json.name);
     });
