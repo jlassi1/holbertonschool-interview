d8114f441ad3378ec921007c117185ce9b58b49b
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-13 12:10:11 +0100

    0-rain.py

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -5,8 +5,25 @@
 
 
 def rain(walls):
-    """function that calculate how much water will be retained after it rains."""
+    """function that calculate how much water
+    will be retained after it rains."""
     if walls == [] or len(walls) == 0:
         return 0
-    water = sum(walls[:-1])
+    water = 0
+    n = len(walls)
+
+    left_wall = [0 for _ in range(n)]
+    right_wall = [0 for _ in range(n)]
+    left_wall[0] = walls[0]
+    right_wall[n - 1] = walls[n - 1]
+
+    for i in range(1, n):
+        left_wall[i] = max(left_wall[i - 1], walls[i])
+    # print(left_wall)
+    for i in range(n - 2, -1, -1):
+        right_wall[i] = max(right_wall[i + 1], walls[i])
+    # print(right_wall)
+    # print(walls)
+    for i in range(n):
+        water += min(left_wall[i], right_wall[i]) - walls[i]
     return water
