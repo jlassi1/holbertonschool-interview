25540d5d3e992fa4d043d25a8d7e03772e25b520
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-22 23:43:21 +0100

    0-heap

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -15,59 +15,60 @@ int tmp = *a;
 	*b = tmp;
 }
 /**
- *heapify-The process of reshaping a binary tree into a Heap data structure
+ *siftDown - sift_down heap sort algorithm
  *@arr: array to sort
- *@n: size of array
- *@i: index
+ *@n: integer
+ *@i: integer
+ *@size: size of array
  *
  */
-void heapify(int arr[], int n, int i)
+
+void siftDown(int *arr, int n, int i, size_t size)
 {
+
 int max = i;
 int leftChild = 2 * i + 1;
 int rightChild = 2 * i + 2;
 
 	/* If left child is greater than root */
-	if (leftChild < n && arr[leftChild] > arr[max])
+	if (leftChild <= n && arr[leftChild] > arr[max] && arr[leftChild] > arr[max])
 		max = leftChild;
 
 	/* If right child is greater than max */
-	if (rightChild < n && arr[rightChild] > arr[max])
+	if (rightChild <= n && arr[rightChild] > arr[max])
 		max = rightChild;
 
 	/* If max is not root*/
 	if (max != i)
 	{
 		swap(&arr[i], &arr[max]);
-
-		/* heapify the affected sub-tree recursively */
-		heapify(arr, n, max);
+		siftDown(arr, i, max, size);
+		print_array(arr, size);
 
 	}
+return;
 }
 
 /**
  * heap_sort-function that sorts an array of integers in ascending
  * order using the Heap sort algorithm
  *
- * @arr: The array to sort it
+ * @array: The array to sort it
  * @size: size of the array
  */
-void heap_sort(int *arr, size_t size)
+void heap_sort(int *array, size_t size)
 {
-int i;
-	/*Rearrange array (building heap)*/
-	for (i = (int)size / 2 - 1; i >= 0; i--)
+int i, j;
+
+	if (!array || size <= 1)
+		return;
+	for (i = (int)size - 1; i > 0; i--)
 	{
-		heapify(arr, (int)size, i);
-		print_array((const int *)arr, size);
-	}
-	/*Extract elements from heap one by one*/
-	for (i = (int)size - 1; i >= 0; i--)
-	{
-		swap(&arr[0], &arr[i]); /*Current root moved to the end*/
-		print_array((const int *)arr, size);
-		heapify(arr, i, 0); /*calling max heapify on the heap reduced*/
-		print_array((const int *)arr, size);
+		for (j = i; j >= 0; j--)
+		{
+			siftDown(array, i, j, size);
+		}
+		print_array(array, size);
+		swap(&array[0], &array[i]);
 	}
 }
