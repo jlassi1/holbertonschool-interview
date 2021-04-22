c8c1ddb15bc7ddf5c0382ed2bb717cd38f5f301f
jlassi1 <khawlajlassi1990@gmail.com>
2021-04-22 23:50:44 +0100

    0-heap_sort.c

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -27,25 +27,25 @@ void siftDown(int *arr, int n, int i, size_t size)
 {
 
 int max = i;
-int leftChild = 2 * i + 1;
-int rightChild = 2 * i + 2;
+int left = 2 * i + 1;
+int right = 2 * i + 2;
 
-	/* If left child is greater than root */
-	if (leftChild <= n && arr[leftChild] > arr[max] && arr[leftChild] > arr[max])
-		max = leftChild;
+/* If left child is greater than root */
+if (left <= n && (right > n || arr[left] > arr[right]) && arr[left] > arr[max])
+	max = left;
 
-	/* If right child is greater than max */
-	if (rightChild <= n && arr[rightChild] > arr[max])
-		max = rightChild;
+/* If right child is greater than max */
+if (right <= n && arr[right] > arr[max] && arr[right] >= arr[left])
+	max = right;
 
-	/* If max is not root*/
-	if (max != i)
-	{
-		swap(&arr[i], &arr[max]);
-		siftDown(arr, i, max, size);
-		print_array(arr, size);
+/* If max is not root*/
+if (max != i)
+{
+	swap(&arr[i], &arr[max]);
+	print_array(arr, size);
+	siftDown(arr, i, max, size);
 
-	}
+}
 return;
 }
 
@@ -68,7 +68,7 @@ int i, j;
 		{
 			siftDown(array, i, j, size);
 		}
-		print_array(array, size);
 		swap(&array[0], &array[i]);
+		print_array(array, size);
 	}
 }
