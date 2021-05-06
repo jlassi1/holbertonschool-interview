2052e1325921029502f5afce892888533f403a74
jlassi1 <khawlajlassi1990@gmail.com>
2021-05-07 00:01:18 +0100

    0-count.py

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -12,24 +12,25 @@ def count_words(subreddit, word_list):
     word_list = [str.lower() for str in word_list]
 
     my_list = get_hot_list(subreddit)
-    print(my_list)
     my_dict = {}
 
     for word in word_list:
         my_dict[word] = 0
+    try:
+        for title in my_list:
+            title_split = title.split(" ")
 
-    for title in my_list:
-        title_split = title.split(" ")
+            for iter in title_split:
+                for iter_split in word_list:
+                    if iter.lower() == iter_split.lower():
+                        my_dict[iter_split] += 1
 
-        for iter in title_split:
-            for iter_split in word_list:
-                if iter.lower() == iter_split.lower():
-                    my_dict[iter_split] += 1
-
-    for key, val in sorted(my_dict.items(), key=lambda x: x[1],
-                           reverse=True):
-        if val != 0:
-            print("{}: {}".format(key, val))
+        for key, val in sorted(my_dict.items(), key=lambda x: x[1],
+                               reverse=True):
+            if val != 0:
+                print("{}: {}".format(key, val))
+    except Exception:
+        return None
 
 
 def get_hot_list(subreddit, hot_list=[], after=None):
@@ -50,6 +51,7 @@ def get_hot_list(subreddit, hot_list=[], after=None):
         dic_json = res.json()
         for title_ in dic_json['data']['children']:
             hot_list.append(title_['data']['title'])
+
         return hot_list
     except Exception:
         return None
