e80d279fab23a9f3e3408eb247e08f74f73722d0
jlassi1 <khawlajlassi1990@gmail.com>
2021-05-20 22:02:07 +0100

    0x15. Infinite Multiplication

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,48 +1,6 @@
 #include "holberton.h"
 
 
-/**
- * lenth - Finds the length of a string.
- * @str: The string to be measured.
- *
- * Return: The length of the string.
- */
-int lenth(char *str)
-{
-	int len = 0;
-
-	while (*str++)
-		len++;
-
-	return (len);
-}
-
-/**
- * create_xarray - Creates an array of chars and initializes it with
- *                 the character 'x'. Adds a terminating null byte.
- * @size: The size of the array to be initialized.
- *
- * Description: If there is insufficient space, the
- *              function exits with a status of 98.
- * Return: A pointer to the array.
- */
-char *create_xarray(int size)
-{
-	char *array;
-	int index;
-
-	array = malloc(sizeof(char) * size);
-
-	if (array == NULL)
-		exit(98);
-
-	for (index = 0; index < (size - 1); index++)
-		array[index] = 'x';
-
-	array[index] = '\0';
-
-	return (array);
-}
 
 /**
  * zeroes_num - Iterates through a string of numbers containing
