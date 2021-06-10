8cf8e22043dd73510a46479a7bb0b6f349f3fd0a
jlassi1 <khawlajlassi1990@gmail.com>
2021-06-10 22:47:46 +0100

    0-merge_sort.c

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -10,44 +10,44 @@
  */
 void merging(int *array, int *arr, int start, int middel, int end)
 {
-	int i = start, j = middel, k = start;
+int i = start, j = middel, k = start;
 
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array((const int *)&arr[start], middel - start);
-	printf("[right]: ");
-	print_array((const int *)&arr[middel], end - middel + 1);
-	while (i < middel && j <= end)
-	{
-		if (arr[i] < arr[j])
-		{
-			array[k] = arr[i];
-			i++;
-			k++;
-		}
-		else
-		{
-			array[k] = arr[j];
-			j++;
-			k++;
-		}
-	}
-	while (i < middel)
-	{
-		array[k] = arr[i];
-		i++;
-		k++;
-	}
-	while (j <= end)
-	{
-		array[k] = arr[j];
-		j++;
-		k++;
-	}
-	printf("[Done]: ");
-	for (i = start; i <= end; i++)
-		arr[i] = array[i];
-	print_array((const int *)&array[start], end - start + 1);
+printf("Merging...\n");
+printf("[left]: ");
+print_array((const int *)&arr[start], middel - start);
+printf("[right]: ");
+print_array((const int *)&arr[middel], end - middel + 1);
+while (i < middel && j <= end)
+{
+if (arr[i] < arr[j])
+{
+array[k] = arr[i];
+i++;
+k++;
+}
+else
+{
+array[k] = arr[j];
+j++;
+k++;
+}
+}
+while (i < middel)
+{
+array[k] = arr[i];
+i++;
+k++;
+}
+while (j <= end)
+{
+array[k] = arr[j];
+j++;
+k++;
+}
+printf("[Done]: ");
+for (i = start; i <= end; i++)
+arr[i] = array[i];
+print_array((const int *)&array[start], end - start + 1);
 }
 
 /**
@@ -59,15 +59,15 @@ void merging(int *array, int *arr, int start, int middel, int end)
  */
 void split_merge(int *array, int *arr, int start, int end)
 {
-	int size, middel;
+int size, middel;
 
-	size = end - start + 1;
-	if (size < 2)
-		return;
-	middel = (size / 2) + start;
-	split_merge(array, arr, start, middel - 1);
-	split_merge(array, arr, middel, end);
-	merging(array, arr, start, middel, end);
+size = end - start + 1;
+if (size < 2)
+return;
+middel = (size / 2) + start;
+split_merge(array, arr, start, middel - 1);
+split_merge(array, arr, middel, end);
+merging(array, arr, start, middel, end);
 }
 
 /**
@@ -77,17 +77,17 @@ void split_merge(int *array, int *arr, int start, int end)
  */
 void merge_sort(int *array, size_t size)
 {
-	int i;
-	int *arr;
+int i;
+int *arr;
 
-	if (!array || size < 2)
-		return;
-	arr = malloc(sizeof(int) * size);
-	if (!arr)
-		return;
-	for (i = 0; i < (int)size; i++)
-		arr[i] = array[i];
+if (!array || size < 2)
+return;
+arr = malloc(sizeof(int) * size);
+if (!arr)
+return;
+for (i = 0; i < (int)size; i++)
+arr[i] = array[i];
 
-	split_merge(array, arr, 0, (int)(size - 1));
-	free(arr);
+split_merge(array, arr, 0, (int)(size - 1));
+free(arr);
 }
