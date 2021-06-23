eb1502c94ceb2d0aadd4058477222c8a248017c4
jlassi1 <khawlajlassi1990@gmail.com>
2021-06-23 10:48:15 +0100

    0-making_change.py

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -7,6 +7,8 @@ Change comes from within
 def makeChange(coins, total):
     """ Given a pile of coins of different values,
     determine the fewest number of coins needed to meet a given amount total"""
+    if (total <= 0):
+        return 0
     coins.sort(reverse=True)
     remains = []
     remain = 0
