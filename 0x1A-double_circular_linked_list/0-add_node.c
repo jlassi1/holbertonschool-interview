dd726380ca66e59e134f5a702bec3385835830f1
jlassi1 <khawlajlassi1990@gmail.com>
2021-07-01 22:58:12 +0100

    0-add_node.c

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -1,41 +1,6 @@
 #include "list.h"
 
 /**
- *add_node_end- adds a new node at the end of a double circular linked list
- *
- *@list: the list to modify 
- *
- *@str: the string to copy into the new node
- *
- *Return: Address of the new node, or NULL on failure
- */
-List *add_node_end(List **list, char *str)
-{
-	List *tmp, *newNode;
-
-	newNode = malloc(sizeof(List));
-	if (!newNode)
-		return (NULL);
-	newNode->str = str;
-	if (!(*list))
-	{
-		*list = newNode;
-		newNode->next = newNode;
-		newNode->prev = newNode;
-	}
-	else
-	{
-		tmp = *list;
-		newNode->next = tmp;
-		newNode->prev = tmp->prev;
-		tmp->prev->next = newNode;
-		tmp->prev = newNode;
-	}
-	return (newNode);
-}
-
-
-/**
  *add_node_begin- adds a new node at the beginning of a double circular linked list
  *
  *@list: the list to modify 
@@ -46,26 +11,67 @@ List *add_node_end(List **list, char *str)
  */
 List *add_node_begin(List **list, char *str)
 {
-	List *tmp, *newNode;
+List *new_start = NULL;
 
-	newNode = malloc(sizeof(List));
-	if (!newNode)
+	new_start = malloc(sizeof(List));
+	if (!new_start)
 		return (NULL);
-	newNode->str = str;
-	if (!*list)
+	new_start->str = strdup(str);
+	if (new_start->str == NULL)
+		return (NULL);
+	if (list == NULL || (*list) == NULL)
 	{
-		*list = newNode;
-		newNode->next = newNode;
-		newNode->prev = newNode;
+		*list = new_start;
+		(*list)->next = *list;
+		(*list)->prev = *list;
+		return (new_start);
 	}
-	else
-	{
-		tmp = *list;
-		newNode->next = tmp;
-		newNode->prev = tmp->prev;
-		tmp->prev->next = newNode;
-		tmp->prev = newNode;
-		*list = newNode;
-	}
-	return (newNode);
+	new_start->next = *list;
+	new_start->prev = (*list)->prev;
+	(*list)->prev = new_start;
+	new_start->prev->next = new_start;
+	(*list) = new_start;
+	return (new_start);
 }
+
+/**
+ *add_node_end- adds a new node at the end of a double circular linked list
+ *
+ *@list: the list to modify 
+ *
+ *@str: the string to copy into the new node
+ *
+ *Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+List *new_end = NULL;
+List *temp;
+
+	new_end = malloc(sizeof(List));
+	if (!new_end)
+		return (NULL);
+	new_end->str = strdup(str);
+	if (new_end->str == NULL)
+		return (NULL);
+	if (list == NULL || (*list) == NULL)
+	{
+		*list = new_end;
+		(*list)->next = *list;
+		(*list)->prev = *list;
+		return (new_end);
+	}
+	temp = *list;
+	while (temp)
+	{
+		if (temp->next == *list)
+			break;
+		temp = temp->next;
+	}
+	new_end->next = temp->next;
+	temp->next = new_end;
+	new_end->prev = temp;
+	new_end->next->prev = new_end;
+	return  (new_end);
+}
+
