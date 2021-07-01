d567a300c6366bb7954ee26e48fbad37e9cb7dcf
jlassi1 <khawlajlassi1990@gmail.com>
2021-07-01 23:02:52 +0100

    betty

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -1,13 +1,14 @@
 #include "list.h"
 
 /**
- *add_node_begin- adds a new node at the beginning of a double circular linked list
+ *add_node_begin- adds a new node at the beginning
+ *of a double circular linked list
  *
  *@list: the list to modify 
  *
  *@str: the string to copy into the new node
  *
- *Return: Address of the new node, or NULL on failure
+ *Return: Address of the newnode,or NULL on failure
  */
 List *add_node_begin(List **list, char *str)
 {
@@ -35,13 +36,14 @@ List *new_start = NULL;
 }
 
 /**
- *add_node_end- adds a new node at the end of a double circular linked list
+ *add_node_end- adds a new node at the end
+ *of a double circular linked list
  *
  *@list: the list to modify 
  *
  *@str: the string to copy into the new node
  *
- *Return: Address of the new node, or NULL on failure
+ *Return: Address of the newnode,or NULL on failure
  */
 List *add_node_end(List **list, char *str)
 {
