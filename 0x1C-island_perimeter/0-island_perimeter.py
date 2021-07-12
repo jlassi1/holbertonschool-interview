8edcd50763cf1c95fd85539a311a227d788f197c
jlassi1 <khawlajlassi1990@gmail.com>
2021-07-12 21:29:46 +0100

    island_perimter

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -4,21 +4,25 @@ Island Perimeter
 """
 
 
+def HelpCount(grid, i, j, row, col):
+    # Check border conditions and if
+    if i < 0 or j < 0 or j >= col or i >= row or grid[i][j] == 0:
+        return 1
+
+    return 0
+
+
 def island_perimeter(grid):
     """function that returns the perimeter of the island described in grid"""
     perim = 0
-    for i in range(len(grid)-1):
-        for j in range(len(grid[0])-1):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
             if grid[i][j] == 0:
                 continue
             else:
-                if grid[i][j-1] == 0:
-                    perim += 1
-                if grid[i][j + 1] == 0:
-                    perim += 1
-                if grid[i+1][j] == 0:
-                    perim += 1
-                if grid[i-1][j] == 0:
-                    perim += 1
+                perim += HelpCount(grid, i + 1, j, len(grid), len(grid[0]))
+                perim += HelpCount(grid, i - 1, j, len(grid), len(grid[0]))
+                perim += HelpCount(grid, i, j + 1, len(grid), len(grid[0]))
+                perim += HelpCount(grid, i, j - 1, len(grid), len(grid[0]))
 
     return perim
