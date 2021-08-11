e2b9164d915212ace8ec7a9dac9782e0bd93497d
jlassi1 <khawlajlassi1990@gmail.com>
2021-08-11 12:09:10 +0100

    regex

---
+++ 0x20-regex/regex.h
@@ -0,0 +1,12 @@
+#ifndef _REGEX_
+#define _REGEX_
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+int regex_match(char const *str, char const *pattern);
+
+#endif /* _REGEX_ */
