d0cf3d8101a5b5c52a071ea7a94be395ca7048a9
jlassi1 <khawlajlassi1990@gmail.com>
2021-08-26 19:49:22 +0100

    prime game

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""0. Prime Game """
+
+
+def is_prime(a):
+    return all(a % i for i in range(2, a))
+
+
+def isWinner(x, nums):
+    """function that determine who the winner of each game"""
+    if x <= 0 or nums == [] or not nums:
+        return None
+    r = min(x, len(nums))
+    winner = {'Maria': 0, 'Ben': 0}
+    i = 0
+    while(r != 0):
+        for i in nums:
+            w = 'Maria'
+            if i == 1:
+                w = 'Ben'
+                winner[w] += 1
+            else:
+                for j in range(2, i + 1):
+                    if is_prime(j):
+                        print(j)
+                        w = 'Maria' if w == 'Ben' else 'Ben'
+                winner[w] += 1
+            # print('-------------------------------------------')
+            x -= 1
+    # print(winner)
+    try:
+        return max(winner, key=winner.get)
+    except BaseException:
+        return None
